826a202942c07a462e62c515157a81d6b77a0775
Andreas Yager <andreas.yager@gmail.com>
2024-04-18 17:26:11 +0200

    calender w/ booked dates

--- src/pages/venuepage/VenuePage.js
+++ src/pages/venuepage/VenuePage.js
@@ -2,28 +2,48 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap";
 import placeholderImg from "../../images/placeholder.jpg";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import "./VenuePage.css";
 
 const VenuePage = () => {
   const { id } = useParams();
   const [venue, setVenue] = useState(null);
+  const [bookedDates, setBookedDates] = useState([]);
 
   useEffect(() => {
     const fetchVenue = async () => {
       try {
         const response = await fetch(
-          `https://v2.api.noroff.dev/holidaze/venues/${id}`
+          `https://v2.api.noroff.dev/holidaze/venues/${id}?_bookings=true`
         );
         if (!response.ok) {
           throw new Error("Network response was not ok");
         }
         const data = await response.json();
         setVenue(data.data);
+        const bookings =
+          data.data.bookings?.map((booking) => ({
+            start: new Date(new Date(booking.dateFrom).setHours(0, 0, 0, 0)),
+            end: new Date(new Date(booking.dateTo).setHours(23, 59, 59, 999)),
+          })) || [];
+        setBookedDates(bookings);
       } catch (error) {
         console.error("Failed to fetch venue:", error);
       }
     };
     fetchVenue();
   }, [id]);
+
+  const tileClassName = ({ date, view }) => {
+    if (view === "month") {
+      for (let booking of bookedDates) {
+        if (date >= booking.start && date <= booking.end) {
+          return "booked";
+        }
+      }
+    }
+  };
 
   if (!venue) {
     return <p>Loading...</p>;
@@ -54,6 +74,7 @@ const VenuePage = () => {
             venue.meta.pets ? "Yes" : "No"
           }`}
         </CardText>
+        <Calendar tileClassName={tileClassName} />
       </CardBody>
     </Card>
   );
